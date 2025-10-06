6d54537eb9e4632beec31f9940826100ad4dd689
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2025-10-06 11:24:42 +0000

    version adjust

--- tables_size.sql
+++ tables_size.sql
@@ -1,38 +1,29 @@
-SELECT 
-    --t.spcname AS "Tbs",
-    n.nspname AS "Schema",
-    c.relname AS "Name",
-    CASE c.relkind 
-        WHEN 'r' THEN 'table' 
-        WHEN 'm' THEN 'materialized view' 
-        WHEN 'p' THEN 'partition table' 
-        END AS "Type",
-    CASE c.relpersistence
-        WHEN 'p' THEN 'permanent'
-        WHEN 'u' THEN 'unlogged'
-    END AS persistence,
-    pg_get_userbyid(c.relowner) AS "Owner",
-    c.relpages,
-    pg_size_pretty(pg_total_relation_size(c.oid)) AS "Total Size",
-    pg_size_pretty(pg_table_size(c.oid)) AS "Size",
-    ltrim(to_char(c.reltuples,'999G999G999G999G999')) AS "Rows",
-    pg_size_pretty(trunc(pg_table_size(c.oid) / c.reltuples)::numeric) AS "Avg Row Size",
-    pg_size_pretty(approx_free_space) AS "Free Size",
-    coalesce(fillfactor::integer,100) AS "Fillfactor",
-    round(approx_free_percent::numeric,2) AS "% Free",
-    round(approx_free_percent::numeric - (100 - coalesce(fillfactor::numeric,100)), 2) AS "% Free - Fillfactor"
-FROM pg_class c
-    -- LEFT JOIN pg_tablespace t ON t.oid = c.reltablespace
-    LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-    LEFT JOIN LATERAL (SELECT option_value AS fillfactor FROM pg_options_to_table(c.reloptions) WHERE option_name = 'fillfactor') o ON true,
-    LATERAL (SELECT * FROM pgstattuple_approx(c.oid)) l
-WHERE 
-        c.relkind IN ('r', 'm','')
-    AND c.relpersistence != 'temporary'
-    AND n.nspname <> 'information_schema'
-    AND n.nspname !~ '^pg_toast'
-    --AND pg_catalog.pg_table_is_visible(c.oid)
---ORDER BY approx_free_space DESC
-ORDER BY c.relpages DESC
-LIMIT 20;
+SELECT
+         current_setting('server_version_num')::int >=  80200  AS pg_82
+        ,current_setting('server_version_num')::int >=  80300  AS pg_83
+        ,current_setting('server_version_num')::int >=  80400  AS pg_84
+        ,current_setting('server_version_num')::int >=  90000  AS pg_90
+        ,current_setting('server_version_num')::int >=  90100  AS pg_91
+        ,current_setting('server_version_num')::int >=  90200  AS pg_92
+        ,current_setting('server_version_num')::int >=  90300  AS pg_93
+        ,current_setting('server_version_num')::int >=  90400  AS pg_94
+        ,current_setting('server_version_num')::int >=  90500  AS pg_95
+        ,current_setting('server_version_num')::int >=  90600  AS pg_96
+        ,current_setting('server_version_num')::int >= 100000  AS pg_10
+        ,current_setting('server_version_num')::int >= 110000  AS pg_11
+        ,current_setting('server_version_num')::int >= 120000  AS pg_12
+        ,current_setting('server_version_num')::int >= 130000  AS pg_13
+        ,current_setting('server_version_num')::int >= 140000  AS pg_14
+        ,current_setting('server_version_num')::int >= 150000  AS pg_15
+        ,current_setting('server_version_num')::int >= 160000  AS pg_16
+        ,current_setting('server_version') AS server_version
+\gset svp_
 
+\set QUIET on
+\timing off
+\if :svp_pg_95
+  \i tables_size_95+.sql 
+\else
+  \qecho - Not supported on version :svp_server_version
+\endif
+\set QUIET off
---
+++ tables_size_95+.sql
@@ -0,0 +1,38 @@
+SELECT 
+    --t.spcname AS "Tbs",
+    n.nspname AS "Schema",
+    c.relname AS "Name",
+    CASE c.relkind 
+        WHEN 'r' THEN 'table' 
+        WHEN 'm' THEN 'materialized view' 
+        WHEN 'p' THEN 'partition table' 
+        END AS "Type",
+    CASE c.relpersistence
+        WHEN 'p' THEN 'permanent'
+        WHEN 'u' THEN 'unlogged'
+    END AS persistence,
+    pg_get_userbyid(c.relowner) AS "Owner",
+    c.relpages,
+    pg_size_pretty(pg_total_relation_size(c.oid)) AS "Total Size",
+    pg_size_pretty(pg_table_size(c.oid)) AS "Size",
+    ltrim(to_char(c.reltuples,'999G999G999G999G999')) AS "Rows",
+    pg_size_pretty(trunc(pg_table_size(c.oid) / nullif(c.reltuples,0))::numeric) AS "Avg Row Size",
+    pg_size_pretty(approx_free_space) AS "Free Size",
+    coalesce(fillfactor::integer,100) AS "Fillfactor",
+    round(approx_free_percent::numeric,2) AS "% Free",
+    round(approx_free_percent::numeric - (100 - coalesce(fillfactor::numeric,100)), 2) AS "% Free - Fillfactor"
+FROM pg_class c
+    -- LEFT JOIN pg_tablespace t ON t.oid = c.reltablespace
+    LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+    LEFT JOIN LATERAL (SELECT option_value AS fillfactor FROM pg_options_to_table(c.reloptions) WHERE option_name = 'fillfactor') o ON true,
+    LATERAL (SELECT * FROM pgstattuple_approx(c.oid)) l
+WHERE 
+        c.relkind IN ('r', 'm','')
+    AND c.relpersistence != 'temporary'
+    AND n.nspname <> 'information_schema'
+    AND n.nspname !~ '^pg_toast'
+    --AND pg_catalog.pg_table_is_visible(c.oid)
+--ORDER BY approx_free_space DESC
+ORDER BY c.relpages DESC
+LIMIT 20;
+
