e24d307a2c71fc158c07d4af8314e45945fcc783
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2025-10-16 14:50:58 +0000

    statements_group_database_total.sql

---
+++ statements_group_database_total.sql
@@ -0,0 +1,37 @@
+\set QUIET on
+\timing off
+
+SELECT
+         current_setting('server_version_num')::int >=  80200  AS pg_82
+        ,current_setting('server_version_num')::int >=  80300  AS pg_83
+        ,current_setting('server_version_num')::int >=  80400  AS pg_84
+        ,current_setting('server_version_num')::int >=  90000  AS pg_90
+        ,current_setting('server_version_num')::int >=  90100  AS pg_91
+        ,current_setting('server_version_num')::int >=  90200  AS pg_92
+        ,current_setting('server_version_num')::int >=  90300  AS pg_93
+        ,current_setting('server_version_num')::int >=  90400  AS pg_94
+        ,current_setting('server_version_num')::int >=  90500  AS pg_95
+        ,current_setting('server_version_num')::int >=  90600  AS pg_96
+        ,current_setting('server_version_num')::int >= 100000  AS pg_10
+        ,current_setting('server_version_num')::int >= 110000  AS pg_11
+        ,current_setting('server_version_num')::int >= 120000  AS pg_12
+        ,current_setting('server_version_num')::int >= 130000  AS pg_13
+        ,current_setting('server_version_num')::int >= 140000  AS pg_14
+        ,current_setting('server_version_num')::int >= 150000  AS pg_15
+        ,current_setting('server_version_num')::int >= 160000  AS pg_16
+        ,current_setting('server_version_num')::int >= 170000  AS pg_17
+        ,current_setting('server_version_num')::int >= 180000  AS pg_18
+        ,current_setting('server_version') AS server_version
+\gset svp_
+
+
+
+\if :svp_pg_17
+  \ir statements_group_database_total_17+.sql
+\elif :svp_pg_13
+  \ir statements_group_database_total_14+.sql
+\else
+  \qecho - Not supported on version :svp_server_version
+\endif
+\timing on
+\set QUIET off
---
+++ statements_group_database_total_14+.sql
@@ -0,0 +1,48 @@
+\x on
+SELECT
+    s.q || ' / ' || current_setting('pg_stat_statements.max') || ' / ' || r.dealloc AS "Number of queryes / Max / Lost"
+FROM  
+ (SELECT count(1) AS q FROM pg_stat_statements) s,
+ pg_stat_statements_info r
+;
+
+\x off
+SELECT 
+    d.datname AS "Database",
+    to_char((sum(total_plan_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'SS.FF6') AS "Avg Plan T",
+    to_char((sum(total_exec_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'SS.FF6') AS "Avg Exec T",
+    to_char((sum(total_plan_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Plan T/Day",
+    to_char((sum(total_exec_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Exec T/Day",
+    to_char((sum(total_plan_time + total_exec_time) / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Total T/Day",
+    trunc(sum(total_plan_time)::numeric * 100 / sum(total_plan_time + total_exec_time)::numeric,1) || ' %'  AS "Plan %",
+    to_char(sum(calls)::numeric / reset_days,'999G999G999') AS "Calls/Day",
+    CASE WHEN current_setting('pg_stat_statements.track') = 'all'
+	THEN to_char(count(1) FILTER (WHERE toplevel = FALSE) / reset_days,'999G999G999')  ELSE 'Disabled' END  AS "N TopLevel/Day",
+    CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
+        THEN to_char((sum(shared_blk_read_time + shared_blk_write_time) / reset_days) 
+            * INTERVAL '1 millisecond','HH24:MI:SS')
+        ELSE 'Disabled' END                                                                                 AS "Shared T/Day",
+    --CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
+    --    THEN to_char((sum(local_blk_read_time + local_blk_write_time)   / reset_days) 
+    --        * INTERVAL '1 millisecond','HH24:MI:SS')
+    --    ELSE 'Disabled' END                                                                               AS "Local  time/Day",
+    CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
+        THEN to_char((sum(temp_blk_read_time + temp_blk_write_time)     / reset_days) 
+            * INTERVAL '1 millisecond','HH24:MI:SS')
+        ELSE 'Disabled' END                                                                                 AS "Temp T/Day",
+    trunc(sum(shared_blks_hit) * 100 / sum(shared_blks_hit + shared_blks_read),1) || ' %' AS "Shared Hit",
+    pg_size_pretty((nullif((sum(shared_blks_hit))::numeric,0)     / reset_days) * current_setting('block_size')::integer) AS "S Hit/Day",
+    pg_size_pretty((nullif((sum(shared_blks_read))::numeric,0)    / reset_days) * current_setting('block_size')::integer) AS "S Read/Day",
+    pg_size_pretty((nullif((sum(shared_blks_written))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "S Write/Day",
+    pg_size_pretty((nullif((sum(shared_blks_dirtied))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "S Dirty/Day",
+    pg_size_pretty((nullif((sum(local_blks_read + local_blks_written))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "Local/Day",
+    pg_size_pretty((nullif((sum(temp_blks_read  + temp_blks_written))::numeric,0)  / reset_days) * current_setting('block_size')::integer) AS "Temp/Day",
+    --to_char(current_timestamp - stats_reset, 'YYYY-MM-DD HH24:MI') AS "Time since last Reset",
+    to_char(stats_reset, 'YYYY-MM-DD HH24:MI') AS "Stats Reset"
+FROM
+    pg_stat_statements s
+    JOIN pg_database d ON d.oid = s.dbid,
+    (SELECT dealloc, EXTRACT(EPOCH FROM current_timestamp - stats_reset)::numeric/(60*60*24) AS reset_days, stats_reset FROM pg_stat_statements_info) AS r
+WHERE datname NOT IN ('template1', 'template0')
+GROUP BY d.datname, r.reset_days, r.stats_reset, r.dealloc
+ORDER BY d.datname;
