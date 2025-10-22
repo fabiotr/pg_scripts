810dc0d3acaa831f41e8b08f43f34a1257cdc681
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2025-10-22 15:27:28 +0000

    statements_total_17+.sql group columns and add rows

--- statements_total_17+.sql
+++ statements_total_17+.sql
@@ -1,32 +1,36 @@
 SELECT
-    count(1) || ' / ' || current_setting('pg_stat_statements.max') || ' / ' || r.dealloc AS "Number of queryes / Max / Lost",
-    to_char((sum(total_plan_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'FF6') AS "Avg Plan time (uS)", 
-    to_char((sum(total_exec_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'FF6') AS "Avg Exec time (uS)", 
-    trunc(sum(total_plan_time)::numeric * 100 / sum(total_plan_time + total_exec_time)::numeric,1) || ' %' AS "Plan %",
-    to_char(sum(calls)::numeric / reset_days,'999G999G999') AS "Total calls/Day",
+    trim(to_char(count(1),'999G999')) || ' - ' || 
+    trim(to_char(current_setting('pg_stat_statements.max')::integer,'9999G999')) || ' - ' || 
+    trim(to_char(r.dealloc,'999G999')) AS "Queries (Tracked - Max - Lost)",
+    to_char((sum(total_plan_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'SS.FF6') || ' - ' ||  
+    to_char((sum(total_exec_time) / reset_days) * INTERVAL '1 millisecond' / (sum(calls)/reset_days),'SS.FF6') AS "Avg (Plan - Exec) time ", 
+    trim(to_char(sum(calls)::numeric / reset_days,'999G999G999G999'))  || ' - ' ||
+    trim(to_char(sum(rows)::numeric  / reset_days,'999G999G999G999'))  AS "(Calls - Rows)/Day",
+    trim(to_char(sum(rows)::numeric / sum(calls)::numeric, '999G999D0')) AS "Rows/Call",
     CASE WHEN current_setting('pg_stat_statements.track') = 'all' 
-	THEN to_char(count(1) FILTER (WHERE toplevel = FALSE) / reset_days,'999G999G999')  ELSE 'Disabled' END AS "Non Toplevel calls/Day", 
+	THEN trim(to_char(count(1) FILTER (WHERE toplevel = FALSE) / reset_days,'999G999G999'))  ELSE 'Disabled' END AS "Non Toplevel calls/Day", 
     CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
         THEN to_char((sum(shared_blk_read_time + shared_blk_write_time) / reset_days) * INTERVAL '1 millisecond','HH24:MI:SS')
-        ELSE 'Disabled' END AS "Shared time/Day",
+        ELSE 'Disabled' END || ' - ' || 
     CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
         THEN to_char((sum(local_blk_read_time + local_blk_write_time)   / reset_days) * INTERVAL '1 millisecond','HH24:MI:SS')
-        ELSE 'Disabled' END AS "Local  time/Day",
+        ELSE 'Disabled' END || ' - ' || 
     CASE WHEN current_setting('track_io_timing')::BOOLEAN = TRUE
         THEN to_char((sum(temp_blk_read_time + temp_blk_write_time)     / reset_days) * INTERVAL '1 millisecond','HH24:MI:SS')
-        ELSE 'Disabled' END AS "Temp   time/Day",
-    to_char((sum(total_plan_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Plan time/Day",
-    to_char((sum(total_exec_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Exec time/Day",
-    to_char((sum(total_plan_time + total_exec_time) / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Total time/Day",
+        ELSE 'Disabled' END AS "(Shared - Local - Temp) time/Day",
+    to_char((sum(total_plan_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') || ' - ' || 
+    to_char((sum(total_exec_time)                   / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') || ' - ' || 
+    to_char((sum(total_plan_time + total_exec_time) / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "(Plan - Exec - Total) time/Day",
+    trunc(sum(total_plan_time)::numeric * 100 / sum(total_plan_time + total_exec_time)::numeric,1) || ' %' AS "Plan time %",
+    pg_size_pretty(trunc(((sum(shared_blks_hit))     / reset_days) * current_setting('block_size')::integer)) || ' - ' ||
+    pg_size_pretty(trunc(((sum(shared_blks_read))    / reset_days) * current_setting('block_size')::integer)) || ' - ' || 
+    pg_size_pretty(trunc(((sum(shared_blks_written)) / reset_days) * current_setting('block_size')::integer)) || ' - ' ||
+    pg_size_pretty(trunc(((sum(shared_blks_dirtied)) / reset_days) * current_setting('block_size')::integer)) AS "Shared (Hit - Read - Write - Dirty)/Day",
     trunc(sum(shared_blks_hit) * 100 / sum(shared_blks_hit + shared_blks_read),1) || ' %' AS "Shared Hit",
-    pg_size_pretty((nullif((sum(shared_blks_hit))::numeric,0)     / reset_days) * current_setting('block_size')::integer) AS "Shared Hit/Day",
-    pg_size_pretty((nullif((sum(shared_blks_read))::numeric,0)    / reset_days) * current_setting('block_size')::integer) AS "Shared Read/Day",
-    pg_size_pretty((nullif((sum(shared_blks_written))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "Shared Write/Day",
-    pg_size_pretty((nullif((sum(shared_blks_dirtied))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "Shared Dirty/Day",
-    pg_size_pretty((nullif((sum(local_blks_read + local_blks_written))::numeric,0) / reset_days) * current_setting('block_size')::integer) AS "Local/Day",
-    pg_size_pretty((nullif((sum(temp_blks_read  + temp_blks_written))::numeric,0)  / reset_days) * current_setting('block_size')::integer) AS "Temp/Day",
-    to_char(current_timestamp - stats_reset, 'YYYY-MM-DD HH24:MI') AS "Time since last Reset",
-    to_char(stats_reset, 'YYYY-MM-DD HH24:MI') AS "Stats Reset"
+    pg_size_pretty((nullif((sum(local_blks_read + local_blks_written))::numeric,0) / reset_days) * current_setting('block_size')::integer) || ' - ' ||
+    pg_size_pretty((nullif((sum(temp_blks_read  + temp_blks_written))::numeric,0)  / reset_days) * current_setting('block_size')::integer) AS "(Local - Temp)/Day",
+    to_char(current_timestamp - stats_reset, 'DD HH24:MI') || ' - ' || 
+    to_char(stats_reset, 'YYYY-MM-DD HH24:MI') AS "Time since reset - Stats Reset"
 FROM
     pg_stat_statements s
     JOIN pg_database d ON d.oid = s.dbid,
