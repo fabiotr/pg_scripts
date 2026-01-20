3e690151cba4c85b75a8ad6f89018807131e3c8b
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-01-20 16:18:57 +0000

    clean_query.sql

--- clean_query.sql
+++ clean_query.sql
@@ -1,6 +1,7 @@
+\prompt 'Type the query ID: ' qid
 SELECT
     regexp_replace(query, '[\n\r\t\u2028\u0020]+',' ','g')
 FROM
     pg_stat_statements
 WHERE
-    queryid = '490026028906706859';
+    queryid = :qid ;
