4b71a6b36475a8c911d82ee99aaf2ff0d6b1282a
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-01-21 17:56:17 +0000

    timing remove

--- kill_active_all_wait_fucking_event.sql
+++ kill_active_all_wait_fucking_event.sql
@@ -19,6 +19,8 @@ SELECT
         ,current_setting('server_version_num')::int >= 140000  AS pg_14
         ,current_setting('server_version_num')::int >= 150000  AS pg_15
         ,current_setting('server_version_num')::int >= 160000  AS pg_16
+        ,current_setting('server_version_num')::int >= 170000  AS pg_17
+        ,current_setting('server_version_num')::int >= 180000  AS pg_18
         ,current_setting('server_version') AS server_version
 \gset svp_
 
