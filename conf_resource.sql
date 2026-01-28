f81873ff6404247861fd0e24e82809794ab9e298
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-01-28 21:24:17 +0000

    typo

--- conf_resource.sql
+++ conf_resource.sql
@@ -1,24 +1,34 @@
-SELECT 
---    category, 
-    name AS "conf", 
-    CASE name 
-	WHEN 'effective_cache_size' 	 	THEN pg_size_pretty(setting::bigint * current_setting('block_size')::bigint)
-	WHEN 'shared_buffers'       	 	THEN pg_size_pretty(setting::bigint * current_setting('block_size')::bigint)
-	WHEN 'temp_buffers'         	 	THEN pg_size_pretty(setting::bigint * current_setting('block_size')::bigint)
-	WHEN 'wal_buffers'          	 	THEN CASE setting WHEN '-1' THEN setting ELSE  pg_size_pretty(setting::bigint  * current_setting('block_size')::bigint) END
-	WHEN 'work_mem'             	 	THEN pg_size_pretty(setting::bigint * 1024)
-	WHEN 'maintenance_work_mem' 	 	THEN pg_size_pretty(setting::bigint * 1024)
-	WHEN 'autovacuum_work_mem'  	 	THEN CASE setting WHEN '-1' THEN setting ELSE pg_size_pretty(setting::bigint * 1024) END
-	WHEN 'vacuum_buffer_usage_limit'        THEN pg_size_pretty(setting::bigint * 1024)	
-	WHEN 'logical_decoding_work_mem' 	THEN pg_size_pretty(setting::bigint * 1024)
-	WHEN 'max_wal_size'         	 	THEN pg_size_pretty(setting::bigint * 1024 * 1024) 	
-	WHEN 'min_wal_size'         	 	THEN pg_size_pretty(setting::bigint * 1024 * 1024)
-	WHEN 'checkpoint_completion_target' 	THEN setting
-	WHEN 'checkpoint_timeout'		THEN setting || unit
-    END AS "Value",
-    source
-FROM pg_settings
-WHERE name IN (
-	'autovacuum_work_mem','vacuum_buffer_usage_limit', 'maintenance_work_mem', 'logical_decoding_work_mem', 'max_wal_size','min_wal_size',
-	'shared_buffers','work_mem','effective_cache_size', 'temp_buffers', 'wal_buffers','checkpoint_timeout', 'checkpoint_completion_target')
-ORDER BY category, name;
+\set QUIET on
+\timing off
+
+SELECT
+         current_setting('server_version_num')::int >=  80200  AS pg_82
+        ,current_setting('server_version_num')::int >=  80300  AS pg_83
+        ,current_setting('server_version_num')::int >=  80400  AS pg_84
+        ,current_setting('server_version_num')::int >=  90000  AS pg_90
+        ,current_setting('server_version_num')::int >=  90100  AS pg_91
+        ,current_setting('server_version_num')::int >=  90200  AS pg_92
+        ,current_setting('server_version_num')::int >=  90300  AS pg_93
+        ,current_setting('server_version_num')::int >=  90400  AS pg_94
+        ,current_setting('server_version_num')::int >=  90500  AS pg_95
+        ,current_setting('server_version_num')::int >=  90600  AS pg_96
+        ,current_setting('server_version_num')::int >= 100000  AS pg_10
+        ,current_setting('server_version_num')::int >= 110000  AS pg_11
+        ,current_setting('server_version_num')::int >= 120000  AS pg_12
+        ,current_setting('server_version_num')::int >= 130000  AS pg_13
+        ,current_setting('server_version_num')::int >= 140000  AS pg_14
+        ,current_setting('server_version_num')::int >= 150000  AS pg_15
+        ,current_setting('server_version_num')::int >= 160000  AS pg_16
+        ,current_setting('server_version_num')::int >= 170000  AS pg_17
+        ,current_setting('server_version_num')::int >= 180000  AS pg_18
+        ,current_setting('server_version') AS server_version
+\gset svp_
+
+
+\if :svp_pg_82
+  \ir conf_resource_82+.sql
+\else
+  \qecho - Not supported on version :svp_server_version
+\endif
+\timing on
+\set QUIET off
