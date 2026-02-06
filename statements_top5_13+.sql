b1653c2d13d5c51fdbd90f21e8bcc8456e5d1aa2
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-02-06 17:25:28 +0000

    load_% adjust

--- statements_top5_13+.sql
+++ statements_top5_13+.sql
@@ -1,6 +1,6 @@
 SELECT 
     row_number() over(order by total_exec_time desc) "N", 
-    trim(to_char(total_exec_time*100/sum(total_exec_time) OVER (),'99D99') || '%') AS "load_%",
+    trim(to_char((total_exec_time + total_plan_time) * 100/sum(total_exec_time + total_plan_time) OVER (),'99D99') || '%') AS "load_%",
     queryid id, 
     array_to_string(regexp_split_to_array(query,'\s+'),' ') AS query
 FROM 
