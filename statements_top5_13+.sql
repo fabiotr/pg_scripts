7c8b9e2f09fca1a43830e7ced87445202ddeefbc
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-02-06 17:37:01 +0000

    restrict to current database

--- statements_top5_13+.sql
+++ statements_top5_13+.sql
@@ -5,6 +5,7 @@ SELECT
     array_to_string(regexp_split_to_array(query,'\s+'),' ') AS query
 FROM 
     pg_stat_statements s
+    JOIN pg_database d ON d.oid = s.dbid
 WHERE datname = current_database()
 ORDER BY total_exec_time + total_plan_time DESC
 LIMIT 5;
