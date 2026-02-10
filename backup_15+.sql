cd0a022b022555459aa485578ee2aba27633e36d
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-02-10 16:12:51 +0000

    format output

--- backup_15+.sql
+++ backup_15+.sql
@@ -1,9 +1,9 @@
 SELECT
-    min(times)              AS start_time,
-    max(times)              AS stop_time,
-    max(times) - min(times) AS duration,
-    now() - min(times)      AS delta_backup_time_from_start,
-    now() - max(times)      AS delta_backup_time_from_finish
+    min(times)              AS "Last start",
+    max(times)              AS "Last stop",
+    max(times) - min(times) AS "Duration",
+--    to_char(now() - min(times), 'DD HH24:MI') AS "Time from last start",
+    to_char(now() - max(times), 'DD HH24:MI') AS "Time from last stop"
 FROM
     ( SELECT
           ((regexp_matches(pg_read_file(waldir || filename), '[0-9]*-[0-9]*-[0-9]* [0-9]*:[0-9]*:[0-9]*',
@@ -11,11 +11,11 @@ FROM
       FROM
           ( SELECT (pg_ls_dir || '/') AS waldir
             FROM pg_ls_dir('.')
-            WHERE pg_ls_dir IN ('pg_wal', 'pg_xlog') ) AS get_waldir_name,
+            WHERE pg_ls_dir = 'pg_wal' ) AS get_waldir_name,
           ( SELECT
                 pg_ls_dir(( SELECT (pg_ls_dir || '/')
                             FROM pg_ls_dir('.')
-                            WHERE pg_ls_dir IN ('pg_wal', 'pg_xlog') )) AS filename ) AS file_list
+                            WHERE pg_ls_dir = 'pg_wal')) AS filename ) AS file_list
       WHERE
           filename LIKE '%.backup' ) AS check_backup_label
 ;
