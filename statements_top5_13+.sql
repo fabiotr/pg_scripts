0d75800d5f794103583d94df471c733b76216f1e
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-02-10 22:29:53 +0000

    fix ranking

--- statements_top5_13+.sql
+++ statements_top5_13+.sql
@@ -1,5 +1,5 @@
 SELECT 
-    row_number() over(order by total_exec_time desc) "N", 
+    row_number() over(order by total_exec_time + total_plan_time DESC) "N", 
     trim(to_char((total_exec_time + total_plan_time) * 100/sum(total_exec_time + total_plan_time) OVER (),'99D99') || '%') AS "load_%",
     queryid id, 
     array_to_string(regexp_split_to_array(query,'\s+'),' ') AS query
