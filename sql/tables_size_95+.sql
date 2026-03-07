db50efb5820ae30dc918a6aa79590b4f2ebf4d52
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-03-07 19:06:27 +0000

    remove free space columns

--- sql/tables_size_95+.sql
+++ sql/tables_size_95+.sql
@@ -16,22 +16,22 @@ SELECT
     pg_size_pretty(pg_total_relation_size(c.oid)) AS "Total Size",
     pg_size_pretty(pg_table_size(c.oid)) AS "Size",
     ltrim(to_char(c.reltuples,'999G999G999G999G999')) AS "Rows",
-    pg_size_pretty(trunc(pg_table_size(c.oid) / nullif(c.reltuples,0))::numeric) AS "Avg Row Size",
-    pg_size_pretty(approx_free_space) AS "Free Size",
-    coalesce(fillfactor::integer,100) AS "Fillfactor",
-    round(approx_free_percent::numeric,2) AS "% Free",
-    round(approx_free_percent::numeric - (100 - coalesce(fillfactor::numeric,100)), 2) AS "% Free - Fillfactor"
+    pg_size_pretty(trunc(pg_table_size(c.oid) / nullif(c.reltuples,0))::numeric) AS "Avg Row Size"
+    --pg_size_pretty(approx_free_space) AS "Free Size",
+    --coalesce(fillfactor::integer,100) AS "Fillfactor",
+    --round(approx_free_percent::numeric,2) AS "% Free",
+    --round(approx_free_percent::numeric - (100 - coalesce(fillfactor::numeric,100)), 2) AS "% Free - Fillfactor"
 FROM pg_class c
     -- LEFT JOIN pg_tablespace t ON t.oid = c.reltablespace
     LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-    LEFT JOIN LATERAL (SELECT option_value AS fillfactor FROM pg_options_to_table(c.reloptions) WHERE option_name = 'fillfactor') o ON true,
-    LATERAL (SELECT * FROM pgstattuple_approx(c.oid)) l
+    --LEFT JOIN LATERAL (SELECT option_value AS fillfactor FROM pg_options_to_table(c.reloptions) WHERE option_name = 'fillfactor') o ON true,
+    --LATERAL (SELECT * FROM pgstattuple_approx(c.oid)) l
 WHERE 
         c.relkind IN ('r', 'm','')
     AND c.relpersistence != 'temporary'
     AND n.nspname <> 'information_schema'
     AND n.nspname !~ '^pg_toast'
-    --AND pg_catalog.pg_table_is_visible(c.oid)
+    AND pg_table_is_visible(c.oid)
 --ORDER BY approx_free_space DESC
 ORDER BY c.relpages DESC
 LIMIT 20;
