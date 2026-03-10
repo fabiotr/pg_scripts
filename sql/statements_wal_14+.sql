eb4aaee83222db2a792cfb1798c91f12f244334e
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-03-10 13:55:34 +0000

    adjust FPI column size

--- sql/statements_wal_14+.sql
+++ sql/statements_wal_14+.sql
@@ -10,7 +10,7 @@ SELECT
     to_char(wal_records::numeric  / calls::numeric, '999G990D9')          AS "Wal/Call",
     pg_size_pretty(trunc(nullif(wal_bytes::numeric/calls,0)))             AS "WAL Size/Call",
     to_char(wal_records::numeric/reset_days, '999G999G999')               AS "WAL Records/Day",
-    to_char(wal_fpi::numeric/reset_days,     '999G999')                   AS "WAL FPI/Day", 
+    to_char(wal_fpi::numeric/reset_days,     '9G999G999')                 AS "WAL FPI/Day", 
     pg_size_pretty(nullif(wal_bytes::numeric/reset_days,0))               AS "WAL Size/Day",
     to_char((total_exec_time + total_plan_time / reset_days) * INTERVAL '1 millisecond', 'HH24:MI:SS') AS "Total Time/Day",
     array_to_string(regexp_split_to_array(substr(query,1,50),'\s+'),' ') ||
