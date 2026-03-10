8b40bae7811e737f671e9f6a4c660e04d0394dfc
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-03-10 13:05:20 +0000

    fix labels

--- sql/views.sql
+++ sql/views.sql
@@ -1,7 +1,10 @@
 \set QUIET on
 \timing off
 
-SELECT schemaname, viewname, viewowner 
+SELECT 
+  schemaname AS "Schema", 
+  viewname   AS "View", 
+  viewowner  AS "Owner"
 FROM pg_views 
 WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
 ORDER BY 1,2;
