05fda9e6e5885fc9883f0056b732470b5e2c1663
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-04-13 14:48:11 +0000

    fix Upper case objects name

--- sql/autovacuum_vacuum_adjust_+_84+.sql
+++ sql/autovacuum_vacuum_adjust_+_84+.sql
@@ -5,9 +5,9 @@
 -- 100 MB SSD write intensive 
 SET lc_numeric = 'C';
 SELECT
-    'ALTER TABLE ' 
-        || n.nspname || '.' || c.relname 
-        || ' SET(autovacuum_vacuum_scale_factor = ' 
+    'ALTER TABLE "' 
+        || n.nspname || '"."' || c.relname 
+        || '" SET(autovacuum_vacuum_scale_factor = ' 
         || CASE 
                 WHEN c.scale < '0.0001' THEN to_char(round(c.scale,5),'0D99999')
                 WHEN c.scale < '0.001'  THEN to_char(round(c.scale,4),'0D9999')
