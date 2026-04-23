78f119e66b83bebf992ff8aa11eaf0f77dc2c81a
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-04-23 17:10:15 +0000

    better columns names

--- sql/object_privileges_list_93+.sql
+++ sql/object_privileges_list_93+.sql
@@ -1,7 +1,6 @@
-SELECT 
-    rolname, 
-    nspname, 
-    relname, 
+SELECT
+    n.nspname AS "Schema",
+    c.relname AS "Object",
     CASE c.relkind
         WHEN 'r' THEN 'TABLE'
         WHEN 'v' THEN 'VIEW'
@@ -11,19 +10,20 @@ SELECT
         WHEN 's' THEN 'special'
         WHEN 'f' THEN 'FOREIGN TABLE'
         WHEN 'p' THEN 'PARTITION TABLE'
-        END AS "Type", 
-    array_to_string(array_agg(acl.privilege_type),', ') priv 
-FROM 
-    pg_class c, 
-    pg_namespace n, 
-    pg_roles r,
-    LATERAL aclexplode(relacl) acl
-WHERE 
-    relacl IS NOT NULL AND 
-    acl.grantee = r.oid AND 
-    c.relnamespace = n.oid AND
-    n.nspname <> 'pg_catalog' AND
-    n.nspname <> 'information_schema' AND
-    n.nspname !~ '^pg_toast'
-GROUP BY rolname, nspname, relname, relkind 
-ORDER BY rolname, nspname, relname;
+        END   AS "Type",
+	o.rolname AS "Owner",
+    r.rolname AS "Grantee",
+    array_to_string(array_agg(acl.privilege_type),', ') AS "Privileges"
+FROM
+    pg_class c
+    JOIN pg_namespace n ON c.relnamespace = n.oid
+    JOIN LATERAL aclexplode(c.relacl) acl ON TRUE
+	JOIN pg_roles r ON acl.grantee = r.oid
+	JOIN pg_roles o ON c.relowner = o.oid
+WHERE
+    c.relacl IS NOT NULL AND
+    n.nspname NOT IN ('pg_catalog', 'information_schema') AND
+    n.nspname !~ '^pg_toast' AND
+	o.oid != r.oid
+GROUP BY n.nspname, c.relname, c.relkind, o.rolname, r.rolname
+ORDER BY n.nspname, c.relname, r.rolname;
