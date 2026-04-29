d58452f76ffa89993b64b68ee019d57d66c8321f
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-04-29 23:04:11 +0000

    add connection by client_addr, application_name and wait_event

---
+++ sql/connections_by_client_addr_92+.sql
@@ -0,0 +1,12 @@
+SELECT
+  client_addr AS "Client Address",
+  state       AS "Status",
+  count(1)    AS "Qt",
+  avg(current_timestamp - query_start) AS "Avg query",
+  max(current_timestamp - query_start) AS "Max query",
+  avg(current_timestamp - xact_start)  AS "Avg xact",
+  max(current_timestamp - xact_start)  AS "Max xact"
+FROM pg_stat_activity
+WHERE pid != pg_backend_pid()
+GROUP BY state, client_addr
+ORDER BY 1,2;
