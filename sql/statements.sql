b69c5e5afd04b50afbc7976a1504fe0e3b8d0e37
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-04-29 19:26:34 +0000

    create install checks

--- sql/statements.sql
+++ sql/statements.sql
@@ -26,247 +26,296 @@ SELECT
 	,current_setting('server_version_num')::int >= 180000  AS pg_18
         ,current_setting('track_io_timing')                    AS track_io
 	,NOT pg_is_in_recovery()                               AS not_standby
+	,current_setting('jit')                                AS jit
+	,current_setting('pg_stat_statements.track_planning')  AS plan
 	,(SELECT CASE WHEN count(1) = 0 THEN TRUE ELSE FALSE END FROM pg_settings WHERE name = 'aurora_compute_plan_id') AS not_aurora
-	,current_setting('jit') AS jit
-	,current_setting('pg_stat_statements.track_planning') AS plan
+	,(SELECT CASE WHEN count(1) = 1 THEN TRUE ELSE FALSE END WHERE current_setting('shared_preload_libraries') LIKE '%pg_stat_statements%') AS lib
 \gset svp_
 
-\qecho
-\qecho '## Statements'
-\qecho
-
-\qecho '### Statements total'
-\qecho
-\pset xheader_width 1
-\x on
-\if :svp_pg_17
-  \ir statements_total_17+.sql
-\elif :svp_pg_14
-  \ir statements_total_14+.sql
+-- Check if pg_stat_statements is installed
+\if :svp_lib
+  \if :svp_pg_91
+    SELECT CASE WHEN count(1) = 0 THEN TRUE ELSE FALSE END AS not_ext FROM pg_extension WHERE extname = 'pg_stat_statements'
+    \gset svp_
+    \if :svp_not_ext
+      \if :svp_not_standby
+        CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+	\set svp_run_ok TRUE
+      \else
+        \qecho '# WARNING'
+        \qecho 'pg_stat_statemens is not installed in this database and can not be installed on Standby cluster'
+	\qecho 'Please install it on Master cluster first'
+ 	\qecho
+        \set svp_run_ok FALSE
+      \endif
+    \else
+      \set svp_run_ok TRUE
+    \endif
+  \else
+    SELECT CASE WHEN count(1) = 0 THEN FALSE ELSE TRUE END AS not_statements FROM pg_class WHERE relname = 'pg_stat_statements'
+    \gset svp_
+    \if svp_not_statements
+      \qecho '# WARNING'
+      \qecho 'pg_stat_staements is not installed in this database'
+      \qecho 'Please install it first'
+      \qecho
+      \set svp_run_ok FALSE
+    \else
+      \set svp_run_ok TRUE
+    \endif
+  \endif
 \else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
-\endif
-\x off
-\pset xheader_width full
-
-\qecho
-\qecho '### Statements resume by total time'
-\qecho
-
-\if   :svp_pg_18
-  \ir statements_resume_18+.sql
-\elif :svp_pg_17
-  \ir statements_resume_17+.sql
-\elif :svp_pg_14
-  \ir statements_resume_14+.sql
-\else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
-\endif
-
-\qecho
-\qecho '### Statements by execution time'
-\qecho
-
-\if   :svp_pg_17
-  \ir statements_time_17+.sql
-\elif :svp_pg_14
-  \ir statements_time_14+.sql
-\elif :svp_pg_13
-  \ir statements_time_13+.sql
-\elif :svp_pg_95
-  \ir statements_time_95+.sql
-\elif :svp_pg_94
-  \ir statements_time_94+.sql
-\elif :svp_pg_84
-  \ir statements_time_84+.sql
-\else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \qecho '# WARINING'
+  \qecho 'pg_stat_statements is not installed on this cluster'
+  \qecho 'Please configure shared_preload_libraries and reboot cluster first'
+  \qecho
+  \set svp_run_ok FALSE
 \endif
 
-\qecho
-\qecho '### Statements by plan time'
-\qecho
+\if :svp_run_ok
+  \qecho
+  \qecho '## Statements'
+  \qecho
 
-\if :svp_plan
+  \qecho '### Statements total'
+  \qecho
+  \pset xheader_width 1
+  \x on
   \if :svp_pg_17
-    \ir statements_plan_17+.sql
+    \ir statements_total_17+.sql
   \elif :svp_pg_14
-    \ir statements_plan_14+.sql
+    \ir statements_total_14+.sql
   \else
     \qecho - pg_stat_statements is not supported on version :svp_server_version
   \endif
-\else
-  \qecho - pg_stat_statements.track_plan is not enabled
-\endif 
+  \x off
+  \pset xheader_width full
 
-\qecho
-\qecho '### Statements by shared I/O'
-\qecho 
+  \qecho
+  \qecho '### Statements resume by total time'
+  \qecho
 
-\if :svp_track_io
-  \if :svp_pg_17
-    \ir statements_shared_17+.sql
+  \if   :svp_pg_18
+    \ir statements_resume_18+.sql
+  \elif :svp_pg_17
+    \ir statements_resume_17+.sql
   \elif :svp_pg_14
-    \ir statements_shared_14+.sql
-  \elif :svp_pg_92
-    \ir statements_shared_92+.sql
+    \ir statements_resume_14+.sql
   \else
-    \qecho - Not supported on version :svp_server_version
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
   \endif
-\else
-  \qecho - track_io_timing is not enabled
-\endif
 
-\qecho
-\qecho '### Statements by local I/O'
-\qecho 
+  \qecho
+  \qecho '### Statements by execution time'
+  \qecho
 
-\if :svp_track_io
-  \if :svp_pg_17
-    \ir statements_local_17+.sql
+  \if   :svp_pg_17
+    \ir statements_time_17+.sql
   \elif :svp_pg_14
-    \ir statements_local_14+.sql
-  \elif :svp_pg_92
-    \ir statements_local_92+.sql
-  \else 
-    \qecho - Not supported on version :svp_server_version
+    \ir statements_time_14+.sql
+  \elif :svp_pg_13
+    \ir statements_time_13+.sql
+  \elif :svp_pg_95
+    \ir statements_time_95+.sql
+  \elif :svp_pg_94
+    \ir statements_time_94+.sql
+  \elif :svp_pg_84
+    \ir statements_time_84+.sql
+  \else
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
   \endif
-\else
-  \qecho - track_io_timing is not enabled
-\endif
 
-\if :svp_not_standby
-  \if :svp_not_aurora
-    \qecho
-    \qecho '### Statements by WAL'
-    \qecho 
+  \qecho
+  \qecho '### Statements by plan time'
+  \qecho
 
-    \if :svp_pg_18
-      \ir statements_wal_18+.sql
-    \elif :svp_pg_17
-      \ir statements_wal_17+.sql
+  \if :svp_plan
+    \if :svp_pg_17
+      \ir statements_plan_17+.sql
     \elif :svp_pg_14
-      \ir statements_wal_14+.sql
-    \elif :svp_pg_13
-      \ir statements_wal_13+.sql
+      \ir statements_plan_14+.sql
+    \else
+      \qecho - pg_stat_statements is not supported on version :svp_server_version
+    \endif
+  \else
+    \qecho - pg_stat_statements.track_plan is not enabled
+  \endif 
+
+  \qecho
+  \qecho '### Statements by shared I/O'
+  \qecho 
+
+  \if :svp_track_io
+    \if :svp_pg_17
+      \ir statements_shared_17+.sql
+    \elif :svp_pg_14
+      \ir statements_shared_14+.sql
+    \elif :svp_pg_92
+      \ir statements_shared_92+.sql
     \else
       \qecho - Not supported on version :svp_server_version
     \endif
+  \else
+    \qecho - track_io_timing is not enabled
   \endif
-\endif
 
-\if :svp_jit
-  \if :svp_pg_11
-    \qecho
-    \qecho '### Statements by Jit'
-    \qecho
-    
+  \qecho
+  \qecho '### Statements by local I/O'
+  \qecho 
+
+  \if :svp_track_io
     \if :svp_pg_17
-      \ir statements_jit_17+.sql
-    \elif :svp_pg_15
-      \ir statements_jit_15+.sql
+      \ir statements_local_17+.sql
+    \elif :svp_pg_14
+      \ir statements_local_14+.sql
+    \elif :svp_pg_92
+      \ir statements_local_92+.sql
+    \else 
+      \qecho - Not supported on version :svp_server_version
+    \endif
+  \else
+    \qecho - track_io_timing is not enabled
+  \endif
+
+  \if :svp_not_standby
+    \if :svp_not_aurora
+      \qecho
+      \qecho '### Statements by WAL'
+      \qecho 
+
+      \if :svp_pg_18
+        \ir statements_wal_18+.sql
+      \elif :svp_pg_17
+        \ir statements_wal_17+.sql
+      \elif :svp_pg_14
+        \ir statements_wal_14+.sql
+      \elif :svp_pg_13
+        \ir statements_wal_13+.sql
+      \else
+        \qecho - Not supported on version :svp_server_version
+      \endif
+    \endif
+  \endif
+
+  \if :svp_jit
+    \if :svp_pg_11
+      \qecho
+      \qecho '### Statements by Jit'
+      \qecho
+    
+      \if :svp_pg_17
+        \ir statements_jit_17+.sql
+      \elif :svp_pg_15
+        \ir statements_jit_15+.sql
+      \endif
+    \else 
+      \qecho - Not supported on version :svp_server_version
     \endif
   \else 
-    \qecho - Not supported on version :svp_server_version
+    \qecho - JIT in not enabled
   \endif
-\else 
-  \qecho - JIT in not enabled
-\endif
 
-\qecho
-\qecho '### Statements by calls'
-\qecho
+  \qecho
+  \qecho '### Statements by calls'
+  \qecho
 
-\if :svp_pg_14
-  \ir statements_calls_14+.sql
-\elif :svp_pg_13
-  \ir statements_calls_13+.sql
-\elif :svp_pg_95
-  \ir statements_calls_95+.sql
-\elif :svp_pg_94
-  \ir statements_calls_94+.sql
-\elif :svp_pg_84
-  \ir statements_calls_84+.sql
+  \if :svp_pg_14
+    \ir statements_calls_14+.sql
+  \elif :svp_pg_13
+    \ir statements_calls_13+.sql
+  \elif :svp_pg_95
+    \ir statements_calls_95+.sql
+  \elif :svp_pg_94
+    \ir statements_calls_94+.sql
+  \elif :svp_pg_84
+    \ir statements_calls_84+.sql
+  \else
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \endif
+
+  \qecho
+  \qecho '### Statements by rows'
+  \qecho
+
+  \if :svp_pg_14
+    \ir statements_rows_14+.sql
+  \elif :svp_pg_13
+    \ir statements_rows_13+.sql
+  \elif :svp_pg_95
+    \ir statements_rows_95+.sql
+  \elif :svp_pg_94
+    \ir statements_rows_94+.sql
+  \elif :svp_pg_84
+    \ir statements_rows_84+.sql
+  \else
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \endif
+
+  \qecho
+  \qecho '### Statements by rows per call'
+  \qecho
+
+  \if :svp_pg_14
+    \ir statements_rows_call_14+.sql
+  \elif :svp_pg_13
+    \ir statements_rows_call_13+.sql
+  \elif :svp_pg_95
+    \ir statements_rows_call_95+.sql
+  \elif :svp_pg_94
+    \ir statements_rows_call_94+.sql
+  \elif :svp_pg_84
+    \ir statements_rows_call_84+.sql
+  \else
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \endif
+
+  \qecho
+  \qecho '### Statements by temp files'
+  \qecho
+
+  \if   :svp_pg_17
+    \ir statements_temp_17+.sql
+  \elif :svp_pg_14
+    \ir statements_temp_14+.sql
+  \elif :svp_pg_13 
+    \ir statements_temp_13+.sql
+  \elif :svp_pg_95
+    \ir statements_temp_95+.sql
+  \elif :svp_pg_94
+    \ir statements_temp_94+.sql
+  \elif :svp_pg_92
+    \ir statements_temp_92+.sql
+  \else
+    \qecho - pg_stat_statements with temp data is not supported on version :svp_server_version
+  \endif
+
+  \qecho
+  \qecho '### Top5 statements by total time with full SQL'
+  \qecho
+
+  \pset xheader_width 1
+  \x on
+  \if :svp_pg_17
+    \ir statements_top5_17+.sql
+  \elif :svp_pg_13 
+    \ir statements_top5_13+.sql
+  \elif :svp_pg_95
+    \ir statements_top5_95+.sql
+  \elif :svp_pg_94
+    \ir statements_top5_94+.sql
+  \elif :svp_pg_84
+    \ir statements_top5_84+.sql
+  \else
+    \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \endif
+  \qecho
+
 \else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
+  \qecho 
+  \qecho 'Execution of pg_stat_statements scripts aborted'
+  \qecho
 \endif
 
-\qecho
-\qecho '### Statements by rows'
-\qecho
-
-\if :svp_pg_14
-  \ir statements_rows_14+.sql
-\elif :svp_pg_13
-  \ir statements_rows_13+.sql
-\elif :svp_pg_95
-  \ir statements_rows_95+.sql
-\elif :svp_pg_94
-  \ir statements_rows_94+.sql
-\elif :svp_pg_84
-  \ir statements_rows_84+.sql
-\else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
-\endif
-
-\qecho
-\qecho '### Statements by rows per call'
-\qecho
-
-\if :svp_pg_14
-  \ir statements_rows_call_14+.sql
-\elif :svp_pg_13
-  \ir statements_rows_call_13+.sql
-\elif :svp_pg_95
-  \ir statements_rows_call_95+.sql
-\elif :svp_pg_94
-  \ir statements_rows_call_94+.sql
-\elif :svp_pg_84
-  \ir statements_rows_call_84+.sql
-\else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
-\endif
-
-\qecho
-\qecho '### Statements by temp files'
-\qecho
-
-\if   :svp_pg_17
-  \ir statements_temp_17+.sql
-\elif :svp_pg_14
-  \ir statements_temp_14+.sql
-\elif :svp_pg_13 
-  \ir statements_temp_13+.sql
-\elif :svp_pg_95
-  \ir statements_temp_95+.sql
-\elif :svp_pg_94
-  \ir statements_temp_94+.sql
-\elif :svp_pg_92
-  \ir statements_temp_92+.sql
-\else
-  \qecho - pg_stat_statements with temp data is not supported on version :svp_server_version
-\endif
-
-\qecho
-\qecho '### Top5 statements by total time with full SQL'
-\qecho
-
-\pset xheader_width 1
-\x on
-\if :svp_pg_17
-  \ir statements_top5_17+.sql
-\elif :svp_pg_13 
-  \ir statements_top5_13+.sql
-\elif :svp_pg_95
-  \ir statements_top5_95+.sql
-\elif :svp_pg_94
-  \ir statements_top5_94+.sql
-\elif :svp_pg_84
-  \ir statements_top5_84+.sql
-\else
-  \qecho - pg_stat_statements is not supported on version :svp_server_version
-\endif
-\qecho
 \x off
 \pset xheader_width full
 \timing on
