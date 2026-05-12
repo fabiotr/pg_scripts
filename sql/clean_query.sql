cced125d945e31b6039135c88669bd2e16c6b719
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-12 22:11:34 +0000

    add \f, \v and \u00A0

--- sql/clean_query.sql
+++ sql/clean_query.sql
@@ -1,6 +1,6 @@
 \prompt 'Type the query ID: ' qid
 SELECT
-    regexp_replace(query, '[\n\r\t\u2028\u0020]+',' ','g')
+    regexp_replace(query, '[\f\v\n\r\t\u2028\u0020\u00A0]+',' ','g')
 FROM
     pg_stat_statements
 WHERE
