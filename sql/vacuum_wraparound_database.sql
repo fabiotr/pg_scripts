e4c950c180722af975923f13b914312e85f4ff51
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-14 15:08:34 +0000

    fix typo

--- sql/vacuum_wraparound_database.sql
+++ sql/vacuum_wraparound_database.sql
@@ -5,7 +5,7 @@
 \elif :svp_pg_93
   \ir vacuum_wraparound_database_93+.sql
 \elif :svp_pg_82
-  \if vacuum_wraparound_database_82+.sql
+  \ir vacuum_wraparound_database_82+.sql
 \else
   \qecho - vacuum_wraparound_table is not supported on version :svp_server_version
 \endif
