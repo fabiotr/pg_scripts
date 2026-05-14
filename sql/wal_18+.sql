407fcd3494a11855ed1bb5254baca40ef6bbc487
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-14 13:47:40 +0000

    add FM on to_char conversion

--- sql/wal_18+.sql
+++ sql/wal_18+.sql
@@ -1,7 +1,7 @@
 SELECT
-    to_char(wal_records::NUMERIC        / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'9999G999G990D9')           AS "Records     / Day",
-    to_char(wal_fpi::NUMERIC            / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'999G999G990D9')            AS "FPI         / Day",
-    to_char(wal_buffers_full::NUMERIC   / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'999G999G990D9')            AS "Buffer full / Day",
-    (pg_size_pretty(wal_bytes           / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24))))                           AS "Total size  / Day",
-    date_trunc('second', current_timestamp - stats_reset)                                                                                AS "Age"
+    to_char(wal_records::NUMERIC        / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'FM9999G999G990D9') AS "Records     / Day",
+    to_char(wal_fpi::NUMERIC            / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'FM999G999G990D9')  AS "FPI         / Day",
+    to_char(wal_buffers_full::NUMERIC   / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'FM999G999G990D9')  AS "Buffer full / Day",
+    (pg_size_pretty(wal_bytes           / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24))))                   AS "Total size  / Day",
+    date_trunc('second', current_timestamp - stats_reset)                                                                        AS "Age"
 FROM pg_stat_wal;
