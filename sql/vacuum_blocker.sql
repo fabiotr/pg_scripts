9b007333dbae0a10db5d4995bd2a06a67a9da037
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-14 21:03:55 +0000

    Add script to ckeck what could be blocking VACUUM

---
+++ sql/vacuum_blocker.sql
@@ -0,0 +1,17 @@
+\ir variables.sql
+
+\if :svp_pg_17
+  \ir vacuum_blocker_17+.sql
+\elif :svp_pg_13
+  \ir vacuum_blocker_13+.sql
+\elif :svp_pg_12
+  \ir vacuum_blocker_12+.sql
+\elif :svp_pg_10
+  \ir vacuum_blocker_10+.sql
+\elif :svp_pg_94
+  \ir vacuum_blocker_94+.sql
+\else
+  \qecho - vacuum_wraparound_table is not supported on version :svp_server_version
+\endif
+\timing on
+\set QUIET off
---
+++ sql/vacuum_blocker_12+.sql
@@ -0,0 +1,58 @@
+SELECT
+    origin AS "Origin",
+    db AS "DB",
+    u AS "User",
+    id AS "Id",
+    status AS "Status",
+    to_char(age,'FM999G999G999') AS "Age",
+    to_char(current_timestamp - duration,'HH24:MI:SS') AS "Duration",
+    to_char(coalesce(txid_current_if_assigned(), txid_current())::text::int8 - txid_snapshot_xmin(txid_current_snapshot())::text::int8,'FM999G999G999') AS "Xids blocking vacuum"
+FROM 
+    (SELECT 
+        'Query'           AS origin, 
+        datname           AS db, 
+        usename           AS u,
+        pid::text         AS id,
+        state             AS status,
+        age(backend_xmin) AS age,
+        xact_start        AS duration
+    FROM pg_stat_activity
+    WHERE 
+        pid != pg_backend_pid()AND 
+        backend_xmin IS NOT NULL
+    UNION
+    SELECT
+        'Prepared Xact',
+        database,
+        owner,
+        gid::text,
+        'N/A',
+        age(transaction),
+        prepared
+    FROM pg_prepared_xacts
+    WHERE transaction IS NOT NULL
+    UNION
+    SELECT
+        'Replication Slot',
+        database,
+        'N/A',
+        slot_name,
+        slot_type || ' ' || CASE WHEN active THEN 'active' ELSE 'inactive' END,
+        greatest(age(catalog_xmin),age(xmin)),
+        NULL
+    FROM pg_replication_slots
+    WHERE greatest(age(catalog_xmin),age(xmin)) IS NOT NULL
+    UNION
+    SELECT
+        'Replication',
+        'N/A',
+        usename,
+        client_addr::text,
+        state,
+        age(backend_xmin),
+        reply_time
+    FROM pg_stat_replication
+    WHERE backend_xmin IS NOT NULL) i
+ORDER BY age DESC, duration DESC
+LIMIT 20;
+
