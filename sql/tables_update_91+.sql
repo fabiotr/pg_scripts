19575ca425f988a0d5acd45740c76b40d9695922
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-20 16:50:17 +0000

    add rows and % rows

--- sql/tables_update_91+.sql
+++ sql/tables_update_91+.sql
@@ -1,15 +1,9 @@
-\set QUIET on
-\t on
-\timing off
---\x on
-SELECT 	'Time since last Reset: ' || to_char(current_timestamp - stats_reset, 'DD-MM-YY hh24:mi') AS "Time since last Reset:"  --, stats_reset
-FROM pg_stat_database WHERE datname = current_database();
-\t off
---\x off
 SELECT 
-	schemaname AS "Schema",
-	relname AS "Table",  
-	lpad(to_char(n_tup_upd / reset_days,'FM999G999G999G999'),15) AS "UPDATEs/Day"
+    schemaname AS "Schema",
+    relname AS "Table",  
+    lpad(to_char(n_live_tup,            'FM999G999G999G999'),15) AS "Rows",
+    lpad(to_char(n_tup_upd / reset_days,'FM999G999G999G999'),15)
+    || lpad(' (' || round(100 * n_tup_upd / reset_days/ n_live_tup) || ' %)',8) AS "UPDATE Rows/Day"
 FROM
     pg_stat_user_tables,
     (SELECT EXTRACT(EPOCH FROM current_timestamp - stats_reset)::numeric/(60*60*24) AS reset_days 
