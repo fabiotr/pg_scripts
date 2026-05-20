5041f86e7cf8481b2a48af94b60e56181fde86e7
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-20 19:55:12 +0000

    add better semantic to Maxwritten

--- sql/bgwriter_17+.sql
+++ sql/bgwriter_17+.sql
@@ -1,6 +1,6 @@
 SELECT
-    pg_size_pretty(trunc((buffers_alloc * current_setting('block_size')::INTEGER) / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24))))       AS "Bytes allocated      / Day",
-    pg_size_pretty(trunc((buffers_clean * current_setting('block_size')::INTEGER) / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24))))       AS "Bytes clean          / Day",
-    to_char(maxwritten_clean::NUMERIC                                  / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)),'FM999G990D9') AS "BG stops by maxpages / Day",
-    date_trunc('second', current_timestamp - stats_reset)                                                                                                      AS "Age"
+    lpad(pg_size_pretty(round((buffers_alloc * current_setting('block_size')::INTEGER) / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)))),7) AS "Allocated / Day",
+    lpad(pg_size_pretty(round((buffers_clean * current_setting('block_size')::INTEGER) / (EXTRACT(EPOCH FROM current_timestamp - stats_reset) / (60*60*24)))),7) AS "Clean     / Day",
+    lpad(coalesce(round(100.0 * maxwritten_clean / (nullif(buffers_clean,0) / current_setting('bgwriter_lru_maxpages')::numeric),1),0) || ' %',7)                AS "Maxwritten",
+    date_trunc('second', current_timestamp - stats_reset)                                                                                                        AS "Age"
 FROM pg_stat_bgwriter;
