01414cd9106896cdb26ecc2eebc1044236bb21cf
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-05-28 23:28:00 +0000

    exclude pg_toast tables

--- sql/tables_changes_91up.sql
+++ sql/tables_changes_91up.sql
@@ -17,8 +17,7 @@ FROM
     pg_stat_all_tables,
     (SELECT EXTRACT(EPOCH FROM current_timestamp - stats_reset)::numeric/(60*60*24) AS reset_days
         FROM pg_stat_database
-        WHERE 
-	  datname = current_database() AND
-          schemaname != 'pg_toast') AS r
+        WHERE datname = current_database()) AS r
+WHERE schemaname != 'pg_toast'
 ORDER BY coalesce(n_tup_ins,0) + coalesce(n_tup_upd,0) + coalesce(n_tup_del,0) DESC
 LIMIT 20;
