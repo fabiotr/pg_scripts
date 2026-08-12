154f2272923b3d17bbf99b7ce50169c62c39e2bf
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-08-12 16:28:18 +0000

    simplify table

--- sql/tables_not_used_90up.sql
+++ sql/tables_not_used_90up.sql
@@ -6,6 +6,6 @@ SELECT
 	pg_size_pretty(pg_table_size(relid)) AS  "Size"
     FROM pg_stat_user_tables 
     WHERE 
-        coalesce(seq_scan,0) + coalesce(idx_scan, 0) = g AND
+        coalesce(seq_scan,0) + coalesce(idx_scan, 0) = 0 AND
         schemaname NOT LIKE 'pg_%'
     ORDER BY schemaname, relname;
