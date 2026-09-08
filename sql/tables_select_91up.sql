60bb7270d5bf36b8c8159c81d624c7958f194a49
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-09-08 19:19:23 +0000

    fix rows count

--- sql/tables_select_91up.sql
+++ sql/tables_select_91up.sql
@@ -1,18 +1,18 @@
 SELECT
-    schemaname AS "Schema",
-    relname AS "Table",
-    lpad(to_char(n_live_tup,            'FM999G999G999G999'),15) AS "Rows",
-    lpad(to_char((coalesce(seq_tup_read,0) + coalesce(idx_tup_fetch,0)) / reset_days,'FM999G999G999G999'),15) 
-        || lpad(' (' || round(100 * (coalesce(seq_tup_read,0) + coalesce(idx_tup_fetch,0)) / 
-	nullif(sum(coalesce(seq_tup_read,0) + coalesce(idx_tup_fetch,0)) OVER (),0),1) || ' %)',9) AS "SELECT Rows/Day"
+    s.schemaname AS "Schema",
+    s.relname AS "Table",
+    lpad(to_char(c.reltuples, 'FM999G999G999G999'),15) AS "Rows",
+    lpad(to_char((coalesce(s.seq_tup_read,0) + coalesce(s.idx_tup_fetch,0)) / reset_days,'FM999G999G999G999'),15)
+        || lpad(' (' || round(100 * (coalesce(s.seq_tup_read,0) + coalesce(s.idx_tup_fetch,0)) /
+        nullif(sum(coalesce(s.seq_tup_read,0) + coalesce(s.idx_tup_fetch,0)) OVER (),0),1) || ' %)',9) AS "SELECT Rows/Day"
 FROM
-    pg_stat_all_tables,
+    pg_stat_all_tables s JOIN
+	pg_class c ON relid = oid,
     (SELECT EXTRACT(EPOCH FROM current_timestamp - stats_reset)::numeric/(60*60*24) AS reset_days
         FROM pg_stat_database
         WHERE datname = current_database()) AS r
-WHERE 
-    schemaname != 'pg_toast' AND
-    coalesce(seq_tup_read,0) + coalesce(idx_tup_fetch,0) > 0
-ORDER BY coalesce(seq_tup_read,0) + coalesce(idx_tup_fetch,0) DESC
+WHERE
+	s.schemaname != 'pg_toast' AND
+    coalesce(s.seq_tup_read,0) + coalesce(s.idx_tup_fetch,0) > 0
+ORDER BY coalesce(s.seq_tup_read,0) + coalesce(s.idx_tup_fetch,0) DESC
 LIMIT 10;
-
