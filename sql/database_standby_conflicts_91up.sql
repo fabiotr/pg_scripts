b2ff178ea2fec71e9fbfc48ac32305fbe25d6805
Fábio Telles Rodriguez <fabio.telles@gmail.com>
2026-09-25 13:03:16 +0000

    fix typo

--- sql/database_standby_conflicts_91up.sql
+++ sql/database_standby_conflicts_91up.sql
@@ -1,14 +1,15 @@
 SELECT
-    d.datname AS "Database",
-    d.conflicts      AS "Total",
-    confl_tablespace AS "Tablespace",
-    confl_lock       AS "Lock",
-    confl_deadlock   AS "Deadlock",
-    confl_snapshot   AS "Snapshot",
-    confl_bufferpin  AS "Bufferpin",
+    sd.datname        AS "Database",
+    sd.conflicts      AS "Total",
+    confl_tablespace  AS "Tablespace",
+    confl_lock        AS "Lock",
+    confl_deadlock    AS "Deadlock",
+    confl_snapshot    AS "Snapshot",
+    confl_bufferpin   AS "Bufferpin",
     age(now(),stats_reset) AS "Age" 
 FROM 
-	pg_stat_database d
-	JOIN pg_stat_database_conflicts c ON d.datid = c.datid
+	pg_database d
+        JOIN pg_stat_database sd ON d.oid = sd.datid
+	JOIN pg_stat_database_conflicts c ON sd.datid = c.datid
 WHERE d.datistemplate IS FALSE
 ORDER BY d.datname;
